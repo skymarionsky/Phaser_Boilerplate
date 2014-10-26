e221adae9c70bd8f0173db525c09fdf6340affb3
Yohei Marion Okuyama <marionwkya@gmail.com>
2014-10-27 08:46:49 +0900

    run after DOM is ready, no scroll

--- js/game/main.js
+++ js/game/main.js
@@ -1,7 +1,11 @@
-(function() {
+$(function() {
+    document.body.addEventListener('touchmove',function(e){
+        e.preventDefault();
+    });
+
     var game = new Phaser.Game(640, 960, Phaser.CANVAS, 'game');
     game.state.add('Boot', App.Boot);
     game.state.add('Preloader', App.Preloader);
     game.state.add('MainMenu', App.MainMenu);
     game.state.start('Boot');
-})();
+});
